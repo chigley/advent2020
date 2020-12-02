141a4b85436d3e3c65f0c687284b49967ba966fd
Charlie Hothersall-Thomas <me@charlie.ht>
2020-12-02 08:39:01 +0000

    Refactor day 2 to reduce duplication

--- day2/day2.go
+++ day2/day2.go
@@ -9,7 +9,17 @@ import (
 
 var passwordLine = regexp.MustCompile(`^(\d+)-(\d+) ([a-z]): ([a-z]+)$`)
 
+type validationFunc func(char uint8, tok1, tok2 int, password string) bool
+
 func Part1(passwordLines []string) (int, error) {
+	return validPasswords(passwordLines, validLetterCount)
+}
+
+func Part2(passwordLines []string) (int, error) {
+	return validPasswords(passwordLines, oneOccurrenceInPositions)
+}
+
+func validPasswords(passwordLines []string, validFunc validationFunc) (int, error) {
 	var validCount int
 	for _, l := range passwordLines {
 		matches := passwordLine.FindStringSubmatch(l)
@@ -17,50 +27,31 @@ func Part1(passwordLines []string) (int, error) {
 			return 0, fmt.Errorf("day2: couldn't parse input line %q", l)
 		}
 
-		lower, err := strconv.Atoi(matches[1])
+		tok1, err := strconv.Atoi(matches[1])
 		if err != nil {
-			return 0, fmt.Errorf("day2: parsing lower bound in %q: %w", l, err)
+			return 0, fmt.Errorf("day2: parsing first token in %q: %w", l, err)
 		}
 
-		upper, err := strconv.Atoi(matches[2])
+		tok2, err := strconv.Atoi(matches[2])
 		if err != nil {
-			return 0, fmt.Errorf("day2: parsing upper bound in %q: %w", l, err)
+			return 0, fmt.Errorf("day2: parsing second token in %q: %w", l, err)
 		}
 
-		char, password := matches[3], matches[4]
-
-		if count := strings.Count(password, char); lower <= count && count <= upper {
+		char, password := matches[3][0], matches[4]
+		if validFunc(char, tok1, tok2, password) {
 			validCount++
 		}
 	}
 	return validCount, nil
 }
 
-func Part2(passwordLines []string) (int, error) {
-	var validCount int
-	for _, l := range passwordLines {
-		matches := passwordLine.FindStringSubmatch(l)
-		if matches == nil {
-			return 0, fmt.Errorf("day2: couldn't parse input line %q", l)
-		}
+func validLetterCount(char uint8, lower, upper int, password string) bool {
+	count := strings.Count(password, string(char))
+	return lower <= count && count <= upper
+}
 
-		i1, err := strconv.Atoi(matches[1])
-		if err != nil {
-			return 0, fmt.Errorf("day2: parsing first index in %q: %w", l, err)
-		}
-		i1--
-
-		i2, err := strconv.Atoi(matches[2])
-		if err != nil {
-			return 0, fmt.Errorf("day2: parsing second index in %q: %w", l, err)
-		}
-		i2--
-
-		char, password := matches[3][0], matches[4]
-
-		if (password[i1] == char && password[i2] != char) || (password[i1] != char && password[i2] == char) {
-			validCount++
-		}
-	}
-	return validCount, nil
+func oneOccurrenceInPositions(char uint8, i1, i2 int, password string) bool {
+	i1--
+	i2--
+	return (password[i1] == char && password[i2] != char) || (password[i1] != char && password[i2] == char)
 }
