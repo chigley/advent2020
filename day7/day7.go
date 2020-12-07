311a833095c631f2d4d3f743f2452bfded5af414
Charlie Hothersall-Thomas <me@charlie.ht>
2020-12-07 08:00:53 +0000

    Simplify control flow

--- day7/day7.go
+++ day7/day7.go
@@ -72,34 +72,35 @@ func ParseRules(in []string) (Contains, ContainedBy, error) {
 		containingColour := Colour(matches[1])
 		if matches[2] == "no other bags" {
 			contains[containingColour] = nil
-		} else {
-			innerMatches := regexpContainedBags.FindAllStringSubmatch(matches[2], -1)
-			if len(innerMatches) == 0 {
+			continue
+		}
+
+		innerMatches := regexpContainedBags.FindAllStringSubmatch(matches[2], -1)
+		if len(innerMatches) == 0 {
+			return nil, nil, fmt.Errorf("day7: unable to parse rule: %q", l)
+		}
+
+		for _, ms := range innerMatches {
+			if len(ms) != 3 {
 				return nil, nil, fmt.Errorf("day7: unable to parse rule: %q", l)
 			}
 
-			for _, ms := range innerMatches {
-				if len(ms) != 3 {
-					return nil, nil, fmt.Errorf("day7: unable to parse rule: %q", l)
-				}
-
-				n, err := strconv.Atoi(ms[1])
-				if err != nil {
-					return nil, nil, fmt.Errorf("day7: unable to parse rule: %q: atoi: %w", l, err)
-				}
-
-				containedColour := Colour(ms[2])
-
-				contains[containingColour] = append(contains[containingColour], BagCount{
-					colour: containedColour,
-					n:      n,
-				})
-
-				if containedBy[containedColour] == nil {
-					containedBy[containedColour] = make(ColourSet)
-				}
-				containedBy[containedColour][containingColour] = struct{}{}
+			n, err := strconv.Atoi(ms[1])
+			if err != nil {
+				return nil, nil, fmt.Errorf("day7: unable to parse rule: %q: atoi: %w", l, err)
 			}
+
+			containedColour := Colour(ms[2])
+
+			contains[containingColour] = append(contains[containingColour], BagCount{
+				colour: containedColour,
+				n:      n,
+			})
+
+			if containedBy[containedColour] == nil {
+				containedBy[containedColour] = make(ColourSet)
+			}
+			containedBy[containedColour][containingColour] = struct{}{}
 		}
 	}
 
