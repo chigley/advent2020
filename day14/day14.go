f9d24611851c19e1e88896e8dfdeb9a656ca724a
Charlie Hothersall-Thomas <me@charlie.ht>
2020-12-14 09:55:59 +0000

    Add day 14 part two

--- day14/day14.go
+++ day14/day14.go
@@ -55,6 +55,43 @@ func Part1(in []string) (uint64, error) {
 	return sum, nil
 }
 
+func Part2(in []string) (uint64, error) {
+	var masks []Mask
+	memory := make(map[uint64]uint64)
+
+	for _, l := range in {
+		tokens := strings.SplitN(l, " = ", 2)
+		if len(tokens) != 2 {
+			return 0, fmt.Errorf("day14: can't parse line: %q", l)
+		}
+
+		lvalue, rvalue := tokens[0], tokens[1]
+		if lvalue == "mask" {
+			masks = permuteMasks(rvalue)
+		} else {
+			var addr uint64
+			if _, err := fmt.Sscanf(lvalue, "mem[%d]", &addr); err != nil {
+				return 0, fmt.Errorf("day14: can't parse token: %q: %w", l, err)
+			}
+
+			n, err := strconv.ParseUint(rvalue, 10, 64)
+			if err != nil {
+				return 0, fmt.Errorf("day14: ParseUint: %w", err)
+			}
+
+			for _, m := range masks {
+				memory[m.Apply(addr)] = n
+			}
+		}
+	}
+
+	var sum uint64
+	for _, n := range memory {
+		sum += n
+	}
+	return sum, nil
+}
+
 func parseMask(mask string) (*Mask, error) {
 	andMask, err := strconv.ParseUint(strings.ReplaceAll(mask, "X", "1"), 2, 64)
 	if err != nil {
@@ -70,4 +107,30 @@ func parseMask(mask string) (*Mask, error) {
 		andMask: andMask,
 		orMask:  orMask,
 	}, nil
+}
+
+func permuteMasks(mask string) []Mask {
+	return permuteMasksHelper(mask, Mask{andMask: (1 << 36) - 1}, 0)
+}
+
+func permuteMasksHelper(mask string, startMask Mask, startIndex int) []Mask {
+	for i := startIndex; i < len(mask); i++ {
+		switch mask[i] {
+		case '1':
+			startMask.orMask |= 1 << (35 - i)
+		case 'X':
+			// One branch where we set bit 35-i to 0
+			ret := permuteMasksHelper(mask, Mask{
+				andMask: startMask.andMask & ^(1 << (35 - i)),
+				orMask:  startMask.orMask,
+			}, i+1)
+
+			// Another branch where we set bit 35-i to 1
+			return append(ret, permuteMasksHelper(mask, Mask{
+				andMask: startMask.andMask,
+				orMask:  startMask.orMask | (1 << (35 - i)),
+			}, i+1)...)
+		}
+	}
+	return []Mask{startMask}
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -17,6 +17,14 @@ var part1Tests = []struct {
 	{"input", 10717676595607},
 }
 
+var part2Tests = []struct {
+	in    string
+	part2 uint64
+}{
+	{"example2", 208},
+	{"input", 3974538275659},
+}
+
 func TestPart1(t *testing.T) {
 	for _, tt := range part1Tests {
 		tt := tt
@@ -33,6 +41,26 @@ func TestPart1(t *testing.T) {
 				t.Error(err)
 			}
 			assert.Equal(t, tt.part1, part1)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	for _, tt := range part2Tests {
+		tt := tt
+		t.Run(tt.in, func(t *testing.T) {
+			t.Parallel()
+
+			in, err := advent2020.ReadStrings(path.Join("testdata", tt.in))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			part2, err := day14.Part2(in)
+			if err != nil {
+				t.Error(err)
+			}
+			assert.Equal(t, tt.part2, part2)
 		})
 	}
 }
